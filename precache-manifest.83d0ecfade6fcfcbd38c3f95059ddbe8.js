4b3bdebb73a2f15e5d6558b47cf418a97deb304b
Anna Gerdii <annagerdiy@gmail.com>
2019-12-24 15:38:31 +0200

    Updates

---
+++ precache-manifest.83d0ecfade6fcfcbd38c3f95059ddbe8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af2e5a44aae8f6d57cd1ec7a8128030",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "c24c0aa03a7be90ee7c0",
+    "url": "/react_tabs-with-router/static/css/main.93a5a09b.chunk.css"
+  },
+  {
+    "revision": "a6ed9298720ddeba0193",
+    "url": "/react_tabs-with-router/static/js/2.605c7544.chunk.js"
+  },
+  {
+    "revision": "c24c0aa03a7be90ee7c0",
+    "url": "/react_tabs-with-router/static/js/main.3059cfd2.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
